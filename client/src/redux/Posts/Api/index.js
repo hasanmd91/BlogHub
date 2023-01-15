6773f8605aa51b9f91e259716e687ed05af44a00
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-15 15:43:08 +0200

    fix url

--- client/src/redux/Posts/Api/index.js
+++ client/src/redux/Posts/Api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const url = "https://memory-social-media.onrender.com/";
+const url = "https://memory-social-media.onrender.com/posts";
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost);
