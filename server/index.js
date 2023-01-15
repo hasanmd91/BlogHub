36c76fef4bb1331b2d052ace894306e0398125d5
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-15 14:49:49 +0200

    at / message

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-app.use("/", (req, res) => {
+app.get("/", (req, res) => {
   res.send("welcome to memory app");
 });
 app.use("/posts", router);
