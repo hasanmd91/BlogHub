4a29c7a207fb5c4296a909d20f7b573b4f3b78d7
hasanmd91 <hasanmd1991@hotmail.com>
2023-01-15 14:41:18 +0200

    deployment

--- server/index.js
+++ server/index.js
@@ -12,6 +12,9 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+app.use("/", (req, res) => {
+  res.send("welcome to memory app");
+});
 app.use("/posts", router);
 
 const PORT = process.env.PORT || 5000;
